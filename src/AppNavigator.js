1bcad849e593540d7b5e4a8a0d46bf4df63a2fb2
Matthew Robinson <robinsonm18@cardiff.ac.uk>
2023-09-27 01:20:03 +0100

    add styling to header and bottom tab

--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -30,27 +30,64 @@ const Tab = createBottomTabNavigator();
 
 function BottomTabNavigation() {
     return (
-            <NavigationContainer>
-                <Tab.Navigator initialRouteName="Home">
-                    <Tab.Screen 
-                        name="Sleep Journey" 
-                        component={AppNavigator} 
-                        options={{
-                            tabBarLabel: 'Home',
-                            tabBarIcon: ({ color, size }) => <FontAwesome name="bed" color={color} size={size} />,
-                        }}
-                    />
-                    <Tab.Screen 
-                        name="Details" 
-                        component={Screen2} 
-                        options={{
-                            tabBarLabel: 'History',
-                            tabBarIcon: ({ color, size }) => <FontAwesome name="moon-o" color={color} size={size} />,
-                        }}
-                    />
-                </Tab.Navigator>
-            </NavigationContainer>
+        <NavigationContainer>
+            <Tab.Navigator initialRouteName="Home"
+            screenOptions={{
+                tabBarActiveTintColor: '#fff',
+                tabBarInactiveTintColor: '#001F3F',
+                tabBarStyle: {
+                    borderTopWidth: 0,  // Removes the border
+                    backgroundColor: '#5E4B8B',  // Set the background color here
+                    paddingTop: 2, 
+                },
+                tabBarLabelStyle: {   // Adjust label position if needed
+                    marginBottom: 6,   // Increase this to move label up
+                },
+            }}
+                >
+                <Tab.Screen 
+                    name="Sleep Journey" 
+                    component={AppNavigator} 
+                    options={{
+                        tabBarLabel: 'Home',
+                        tabBarIcon: ({ color, size }) => <FontAwesome name="bed" color={color} size={size} />,
+                        tabBarItemStyle: {
+                            backgroundColor: '#5E4B8B',
+                        },
+                        headerStyle: {
+                            backgroundColor: '#5E4B8B',
+                        },
+                        headerTintColor: '#fff',
+                        headerTitleStyle: {
+                            fontWeight: 'bold',
+                        },
+                    }}
+                />
+                <Tab.Screen 
+                    name="Details" 
+                    component={Screen2} 
+                    options={{
+                        tabBarLabel: 'History',
+                        tabBarIcon: ({ color, size }) => <FontAwesome name="moon-o" color={color} size={size} />,
+                        tabBarItemStyle: {
+                            backgroundColor: '#5E4B8B',
+                        },
+                        tabBarItemStyle: {
+                            backgroundColor: '#5E4B8B',
+                        },
+                        headerStyle: {
+                            backgroundColor: '#5E4B8B',
+                        },
+                        headerTintColor: '#fff',
+                        headerTitleStyle: {
+                            fontWeight: 'bold',
+                        },
+                    }}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
     );
 }
+
 
 export default BottomTabNavigation;
